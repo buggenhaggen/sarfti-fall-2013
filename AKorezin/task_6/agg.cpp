0e732bb5a79908d5a17e52472e0b7e8c1002ed86
AKorezin <source-email@yandex.ru>
2013-10-25 01:36:36 +0400

    reworking agg.cpp; public: A replaced with private: *A; class A is indepent from B

--- AKorezin/task_6/agg.cpp
+++ AKorezin/task_6/agg.cpp
@@ -6,7 +6,7 @@ class A
 {
 public:
 void setx(int x1){x=x1;};
-A(void){x=1;};
+A(int x1=1):x(x1){};
 int getx(void){return x;};
 private:
 int x;
@@ -15,20 +15,24 @@ int x;
 class B
 {
 private:
-int x;
+int y;
+A *agg;
 public:
-A agg;
-void setx(int x1){x=x1;};
-B(void){x=2;};
-int getx(void){return x;};
+void sety(int y1){y=y1;};
+B(A *agg1=NULL,int y1=1):agg(agg1),y(y1){};
+int gety(void){return y;};
+int getx(void){return agg->getx();};
+void setx(int x1){agg->setx(x1);};
 };
 
 int main(void)
 {
-B agg1;
-cout << agg1.getx()<<" "<< agg1.agg.getx()<<endl;
-agg1.setx(4);
-agg1.agg.setx(3);
-cout << agg1.getx()<<" "<< agg1.agg.getx()<<endl;
+A *agg1 = new A(2);
+B agg2(agg1,2);
+cout << agg2.gety()<<" "<< agg2.getx()<<endl;
+agg2.setx(3);
+agg2.sety(3);
+cout << agg2.gety()<<" "<< agg2.getx()<<endl;
+delete agg1;
 return 0;
 }
