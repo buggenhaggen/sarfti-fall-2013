8c2bdc2c7e03f67cb9d8f45d2b3a0096fbcacac7
AnnPonomareva <AnnPonomareva94@gmail.com>
2013-11-11 13:39:37 +0400

    files have been added

---
+++ task_9/test_int.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(void)
+{
+        int num_1=1,num_2=0;
+        rez=num_1/num_2;
+        cout<<rez<<endl;
+        
+        num_1=0;
+        rez=num_1/num_2;
+        cout<<rez<<endl;        
+        
+        num_1=-1;
+        rez=num_1/num_2;
+        cout<<rez<<endl;
+}
