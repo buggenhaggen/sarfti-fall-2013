958f23098d2215f81b27a1ecdf09a023bce26663
AnnPonomareva <AnnPonomareva@gmail.com>
2013-11-12 08:48:16 -0800

    Update test_double.cpp

--- task_9/test_double.cpp
+++ task_9/test_double.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 int main(void)
 {
-        double num_1=1,num_2=0;
-        rez=num_1/num_2;
+        double num1=1,num2=0;
+        double rez;
+        rez=num1/num2;
         cout<<rez<<endl;
         
         num_1=0;
-        rez=num_1/num_2;
+        rez=num1/num2;
         cout<<rez<<endl;        
         
-        num_1=-1;
-        rez=num_1/num_2;
+        num1=-1;
+        rez=num_1/num2;
         cout<<rez<<endl;
 }
