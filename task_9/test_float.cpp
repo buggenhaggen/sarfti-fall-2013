5fe7c7d91e59923174872e47200093578308b8fd
AnnPonomareva <AnnPonomareva@gmail.com>
2013-11-12 08:53:30 -0800

    Update test_float.cpp

--- task_9/test_float.cpp
+++ task_9/test_float.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <math.h>
+#include <conio.h>
 
 using namespace std;
 int main(void)
 {
-        float num_1=1,num_2=0;
-        rez=num_1/num_2;
+        float num1=1,num2=0;
+        float rez;
+        rez=num1/num2;
         cout<<rez<<endl;
         
-        num_1=0;
-        rez=num_1/num_2;
+        num1=0;
+        rez=num1/num2;
         cout<<rez<<endl;        
         
-        num_1=-1;
-        rez=num_1/num_2;
+        num1=-1;
+        rez=num1/num2;
         cout<<rez<<endl;
+        getch();
 }
