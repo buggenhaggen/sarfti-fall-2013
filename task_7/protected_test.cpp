5d6673589617a5f08a777cdd3c22a87dc610ff66
AnnPonomareva <AnnPonomareva@gmail.com>
2013-11-13 03:36:35 -0800

    Update protected_test.cpp

--- task_7/protected_test.cpp
+++ task_7/protected_test.cpp
@@ -3,33 +3,33 @@
 
 using namespace std;
 
-class Class_1
-  {
-  protected: //защищенные данные
-  int num1;
-  friend int function1_num2(Class_1 &);//дружествен. ф.
-  void function2_num2(int num2)//описание ф
-  {
-    num1=num2+num2*2;
-   };
-  public://публичные данные
-   int function1_num1(void)
-      {
-        return num1;
-      };
-  void function2_num1(int num2)
-  {
-    num1=num2+num2*2;
-   };
-  };
-  int function1_num2(Class_1 &object)
-  {
-    return object.num1;
-  }
-  int main(void)
-  {
-    Class_1 x;//создание объекта х, пренадлежащего классуClass_1
-    int f=function1_num2(x);
-    return 0;
-    getch();
-   }
+class ClassA
+{
+ protected: 
+ int a;
+ friend int one_b(ClassA &);
+ void two_b(int b)
+ {
+  a=b+b;
+ };
+ public:
+ int one_a(void)
+ {
+  return a;
+ };
+ void two_a(int b)
+ {
+  a=b+b;
+ };
+};
+ int one_b(ClassA &object)
+ {
+  return object.a;
+ }
+ int main(void)
+ {
+ ClassA x;
+ int y=one_b(x);
+ getch();
+ return 0;
+ }
