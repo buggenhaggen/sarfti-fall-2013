00d6ac9deee9dd301dba6c3dd981a404b834cd9e
Moskaevaolga <mas-ol@mail.ru>
2014-01-08 22:18:59 +0400

    files have been added

---
+++ task_9/i.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+int main(void)
+{
+        int x=1,y=0;
+        int rez;
+        rez=x/y;
+        cout<<rez<<endl;
+        
+        x=0;
+        rez=x/y;
+        cout<<rez<<endl;        
+        
+        x=-1;
+        rez=x/y;
+        cout<<rez<<endl;
+        getch();
+}
