54ac5b6be00c2a79743b45f784bb3d86fb309c03
AnnPonomareva <AnnPonomareva@gmail.com>
2014-01-08 05:40:57 -0800

    Update protected.cpp

--- task_7/protected.cpp
+++ task_7/protected.cpp
@@ -7,47 +7,48 @@ using namespace std;
 class Parent
 {
 protected:
-	int x;
+  int x;
 public:
 	
-	Parent(void)
-	{
-	  x=1;
-	};
+  Parent(void)
+  {
+   x=1;
+  };
 	
-	void setx(int x1)
-	{
-	  x=x1+3;
-	};
+  void setx(int x1)
+  {
+   x=x1+3;
+  };
 	
-	int getx(void)
-	{
-	  return x;
-	};
+  int getx(void)
+  {
+   return x;
+  };
 };
+
 class Child:public Parent
 {
 public:
-	int print_x(void)
-	{
-	  return x;
-	}
+  int print_x(void)
+  {
+   return x;
+  }
 };
 
-	int main(void)
+int main(void)
 {
 	
-	Child obj;
-	cout<<"enter size of array: "<<endl;
-	int size;
-	cin>>size;
-	for(int i=0; i<size; i++)
-	{
-	 obj.setx(i);
-	 cout<<obj.getx()<<" ";
-	}
-	cout<<endl<<"print protected-data x: ";
-	cout<<obj.print_x()<<" ";
-	getch();
-	return 0;
+Child obj;
+cout<<"enter size of array: "<<endl;
+int size;
+cin>>size;
+for(int i=0; i<size; i++)
+ {
+  obj.setx(i);
+  cout<<obj.getx()<<" ";
+ }
+cout<<endl<<"print protected-data x: ";
+cout<<obj.print_x()<<" ";
+getch();
+return 0;
 }
